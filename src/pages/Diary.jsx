bffd4d3653956f7eff9f35b3a0a61f1ac5a34f89
JW <kingcoding2298@gmail.com>
2024-11-06 10:47:08 +0900

    12.5) 페이지 라우팅 4. 동적 경로 : Query String
    : 검색어 등의 자주 변경되는 값을 주소로 명시하기 위해 사용
    	? 뒤에 변수명과 값 명시
    	~/search?q = 검색어

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 const Diary = () => {
   const params = useParams();
-  console.log(params);
+  // http://localhost:5173/diary/1
+  console.log(params); // {id : '1'}
   return <div>{params.id}번 일기 입니다.</div>;
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import { useSearchParams } from "react-router-dom";
 const Home = () => {
+  const [params, setParams] = useSearchParams();
+  // http://localhost:5173/?value=hello
+  console.log(params); // {URLSearchParams {...}
+  console.log(params.get("value")); //hello
   return <>Home</>;
 };
 
